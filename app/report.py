a7812c6906add239348f514e40f9e458ebe61b3c
Alessandro Nardecchia <{ID}+{username}@users.noreply.github.com>
2024-05-31 15:36:42 +0200

    Added a final report by a llm

--- app/report.py
+++ app/report.py
@@ -4,7 +4,7 @@ import json
 from ollama import Client
 
 LLMS = os.environ.get("LLMS").split(" ")
-LLM_REPORT = os.environ.get("LLM_REPORT")
+LLM_REPORT = os.environ.get("LLM_REPORT").split(" ")
 OLLAMA_HOST = os.environ.get("OLLAMA_HOST")
 OLLAMA_PORT = int(os.environ.get("OLLAMA_PORT"))
 JSON_DATA_PATH = os.environ.get("JSON_DATA_PATH")
@@ -46,9 +46,7 @@ if __name__ == "__main__":
     json_data = json.loads(json_string)
 
     now = datetime.datetime.now().replace(microsecond=0)
-
-    reports = []
-    llms_used = 0
+    
 
     for LLM in LLMS:
         if len(LLM) > 0:
@@ -113,53 +111,3 @@ if __name__ == "__main__":
                             part["path"]
                         )
                     )
-
-            reports.append(report_path)
-            llms_used += 1
-
-    # write final merged report
-
-    final_report_data = ""
-    
-    final_report_path = "/storage/output/%s/final_%s.txt" % (
-        now.isoformat(),
-        LLM_REPORT
-    )
-
-    for r in reports:
-        f = open(r, "r").read()
-        final_report_data += "%s\n" % (f)
-
-
-    prompt = """
-        Givin this context: A data composed by %s output different security report of ai models separated by multiple hashtag string.
-        write a report that compares results of every single report and summerize every vulnerability found,
-        avoiding duplicates and givin it a score out of ten in line. On the end write what report is more complete and accurate.
-        \n\n%s
-    """ % (
-        llms_used,
-        final_report_data
-    )
-
-    final_prompt_path = "/storage/output/%s/final_prompt_%s.txt" % (
-        now.isoformat(),
-        LLM_REPORT
-    )
-
-    write_to_report(
-        final_prompt_path,
-        prompt
-    )
-
-    response = do_chat(
-        LLM,
-        prompt
-    )
-    write_to_report(
-        final_report_path,
-        "\n%s\n\n" % (
-            response
-        )
-    )
-
-        
